50c71dc74de7b6219e6c971c7c3da2daae9df7d7
Mannem Vamsi <vamsimannem002@gmail.com>
2026-09-22 11:06:04 +0530

    Update API_URL to use environment variable

--- client/src/services/productService.ts
+++ client/src/services/productService.ts
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:5000/api";
-
+cconst API_URL = import.meta.env.VITE_API_BASE_URL|| "http://localhost:5000/api";
 export interface Product {
   _id: string;
   name: string;
